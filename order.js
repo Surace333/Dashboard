1a48d18bb9300bcb00fa6cca9e326449d5322e78
Surace333 <99582004+Surace333@users.noreply.github.com>
2022-03-08 14:17:02 +0530

    Add files via upload

---
+++ order.js
@@ -0,0 +1,48 @@
+const orders = [
+
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+
+    {
+      productName: 'LARVENDER KF102 Drone',
+      productNumber: '36378',
+      paymentStatus: 'Refunded',
+      shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+
+    {
+        productName: 'GPS 4K Drone',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'DJI Air 25',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Lozenga Drone',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
